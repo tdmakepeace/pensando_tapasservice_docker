cc01eee06e999f8bef23ac6c1644f26ecdab912a
Toby Makepeace <tdmakepeace@gmail.com>
2021-01-02 22:49:47 +0000

    variables

--- src/start.sh
+++ src/start.sh
@@ -12,9 +12,9 @@ if [ -d "$DIR" ]
         sleep 5s
         service mysql stop 
         #cp /app/src/mastervariables.py /app/PenTapasaService/variables.py
-        mv /var/lib/mysql/PenTapasaService /app/PenTapasaService
+        mv /var/lib/mysql/TapAsAService /app/PenTapasaService
         ln -s /app/PenTapasaService/TapAsAService /var/lib/mysql/TapAsAService
-        chmod 777 /app/PenTapasaService
+		chmod 777 /app/PenTapasaService
         mkdir /app/PenTapasaService/backups
         echo '[mysqld]' >> /etc/mysql/my.cnf
         echo 'bind-address = 0.0.0.0' >> /etc/mysql/my.cnf
@@ -23,13 +23,16 @@ if [ -d "$DIR" ]
         service mysql start
 		sleep 5s
 		python3 /app/src/TapasaService.py > /app/PenTapasaService/debug.txt  2>&1
+		mv /app/src/variables.py /app/PenTapasaService/variables.py
+		ln -s /app/PenTapasaService/variables.py /app/src/variables.py
     else
         service mysql start ; mysql < /app/src/db/PenTapasaService.sql 
         sleep 5s
         service mysql stop 
         rm -R /var/lib/mysql/TapAsAService
         ln -s /app/PenTapasaService/TapAsAService /var/lib/mysql/TapAsAService
-        chmod 777 /app/PenTapasaService
+		ln -s /app/PenTapasaService/variables.py /app/src/variables.py
+		chmod 777 /app/PenTapasaService
         echo '[mysqld]' >> /etc/mysql/my.cnf
         echo 'bind-address = 0.0.0.0' >> /etc/mysql/my.cnf
         echo 'socket = /var/lib/mysql/mysql.sock' >> /etc/mysql/my.cnf
