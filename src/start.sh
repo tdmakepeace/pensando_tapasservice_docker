a1da86f9627ccf41965f1116c3484f994db670b4
Toby Makepeace <tdmakepeace@gmail.com>
2021-01-06 12:07:05 +0000

    Handle container update

--- src/start.sh
+++ src/start.sh
@@ -4,6 +4,7 @@
 
 DIR=/app/PenTapasaService
 FILE=/app/PenTapasaService/variables.py
+FILE2=/app/PenTapasaService/delete_me_if_upgrade.tmp
 if [ -d "$DIR" ] 
     then
     if [ ! -f "$FILE" ]
@@ -25,20 +26,27 @@ if [ -d "$DIR" ]
 		python3 /app/src/TapasaService.py > /app/PenTapasaService/debug.txt  2>&1
 		mv /app/src/variables.py /app/PenTapasaService/variables.py
 		ln -s /app/PenTapasaService/variables.py /app/src/variables.py
+		sleep 5s
+		echo 'original' > /app/PenTapasaService/delete_me_if_upgrade.tmp
     else
-		echo 'original' > /app/PenTapasaService/temp.txt
-        #service mysql start ; mysql < /app/src/db/PenTapasaService.sql 
-        #sleep 5s
-        #service mysql stop 
-        #rm -R /var/lib/mysql/TapAsAService
-        #ln -s /app/PenTapasaService/TapAsAService /var/lib/mysql/TapAsAService
-		#ln -s /app/PenTapasaService/variables.py /app/src/variables.py
-		#chmod 777 /app/PenTapasaService
-        #echo '[mysqld]' >> /etc/mysql/my.cnf
-        #echo 'bind-address = 0.0.0.0' >> /etc/mysql/my.cnf
-        #echo 'socket = /var/lib/mysql/mysql.sock' >> /etc/mysql/my.cnf
-        #sleep 5s
-        service mysql start
+		if [ ! -f "$FILE2" ]
+			then
+				echo 'updated' > /app/PenTapasaService/delete_me_if_upgrade.tmp
+				service mysql start ; mysql < /app/src/db/PenTapasaService.sql 
+				sleep 5s
+				service mysql stop 
+				rm -R /var/lib/mysql/TapAsAService
+				ln -s /app/PenTapasaService/TapAsAService /var/lib/mysql/TapAsAService
+				ln -s /app/PenTapasaService/variables.py /app/src/variables.py
+				chmod 777 /app/PenTapasaService
+				echo '[mysqld]' >> /etc/mysql/my.cnf
+				echo 'bind-address = 0.0.0.0' >> /etc/mysql/my.cnf
+				echo 'socket = /var/lib/mysql/mysql.sock' >> /etc/mysql/my.cnf
+				sleep 5s
+				service mysql start
+			else
+				service mysql start
+		fi
     fi
     python3 /app/src/TapasaService.py > /app/PenTapasaService/debug.txt  2>&1
     # python3 /app/src/TapasaService.py > /dev/null  2>&1
